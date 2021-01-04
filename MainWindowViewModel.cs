440a7499a232fb60c6644198d8e9c29ea228ea31
Jogger <beltchikov@gmx.de>
2021-01-04 15:01:31 +0100

    Intermediate changes

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -11,17 +11,20 @@ namespace IbDataTool
     public class MainWindowViewModel : DependencyObject
     {
         public static readonly DependencyProperty PortIbProperty;
+        public static readonly DependencyProperty ConnectionStringProperty;
 
         public RelayCommand CommandImportData { get; set; }
 
         static MainWindowViewModel()
         {
             PortIbProperty = DependencyProperty.Register("PortIb", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(0));
+            ConnectionStringProperty = DependencyProperty.Register("ConnectionString", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(string.Empty));
         }
 
         public MainWindowViewModel()
         {
             PortIb = 4001;
+            ConnectionString= "TODO";
 
             CommandImportData = new RelayCommand((p) => ImportData(p));
         }
@@ -35,13 +38,22 @@ namespace IbDataTool
             set { SetValue(PortIbProperty, value); }
         }
 
+
+
+        public string ConnectionString
+        {
+            get { return (string)GetValue(ConnectionStringProperty); }
+            set { SetValue(ConnectionStringProperty, value); }
+        }
+
+
         /// <summary>
         /// ImportData
         /// </summary>
         /// <param name="p"></param>
         private void ImportData(object p)
         {
-            
+
         }
 
 
