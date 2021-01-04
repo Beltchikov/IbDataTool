6ee03b8f3e1d7c3f8fb86383a8b284136faa4cc7
Jogger <beltchikov@gmx.de>
2021-01-04 14:48:28 +0100

    Add project files.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace IbDataTool
+{
+    /// <summary>
+    /// MainWindowViewModel
+    /// </summary>
+    public class MainWindowViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty PortIbProperty;
+
+        static MainWindowViewModel()
+        {
+            PortIbProperty = DependencyProperty.Register("PortIb", typeof(int), typeof(MainWindowViewModel), new PropertyMetadata(0));
+        }
+
+        public MainWindowViewModel()
+        {
+            PortIb = 4001;
+        }
+
+        /// <summary>
+        /// PortIb
+        /// </summary>
+        public int PortIb
+        {
+            get { return (int)GetValue(PortIbProperty); }
+            set { SetValue(PortIbProperty, value); }
+        }
+
+
+
+
+    }
+}
